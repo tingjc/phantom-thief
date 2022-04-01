24c4d5eb3ed45b0a3dc2f23cfedb90a096054eda
tingjc <jean.claude.ting@gmail.com>
2022-04-01 13:32:05 -0400

    added api call

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -28,8 +28,38 @@
 
 const galleryApp = {};
 
+const url = new URL('https://collectionapi.metmuseum.org/public/collection/v1/search')
+        url.search = new URLSearchParams(
+            {
+                q : "sunflowers",
+                
+            }
+        )
+
+        console.log(url);
+        
+
+//This is the function that makes the call
+galleryApp.apiCall = function() {
+    fetch(url).then(function(response) {
+    return response.json();
+    //this returns a json of objectIDs
+}).then(function(jsonResponse) {
+    console.log(jsonResponse);
+    
+
+    // To show the object, the url =  https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
+    const first = jsonResponse.objectIDs[0];
+    const second = jsonResponse.objectIDs[1];
+    console.log(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${first}`);
+    console.log(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${second}`);
+
+})
+};
+
 galleryApp.init = function() {
     console.log("Hello")
+    galleryApp.apiCall();
 }
 
 galleryApp.init();
