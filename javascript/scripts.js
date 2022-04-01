ec84a61c70c6c9029d29b6d21df117cddd2b4d2b
tingjc <jean.claude.ting@gmail.com>
2022-04-01 11:33:21 -0400

    added more pseudocode

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,25 +1,30 @@
 //Pseudo - code
-// Get user input from dropdown menus 
+// Get user input from dropdown menus
+    // store selections as a variable
 // Use user input for API call parameters
+
 // MOMA search url = https://collectionapi.metmuseum.org/public/collection/v1/search
 // parameters:
-// -- q : [predetermined keyword]
+// -- q : [dropdown menu item]
 // -- dateBegin : [start date]
 // -- dateEnd : [end date]
 // -- hasImages : true
 
 // MOMA search call gives an array of objectIDs
-// Get a selection of IDs from the array
-// for each item in that selection
-// -- create an html element
-// -- append info found in MOMA object json:
-// MOMA object url =  https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
-// ---- img src, 
-// ---- title, 
-// ---- artist name, 
-// ---- medium, 
-// ---- link to moma/wiki entry
-// -- display element on page
+    // Get top 12 IDs from the array
+
+    // for each item in that array of 12
+        // -- create an li element
+        // -- append li to ul
+        
+        // -- append info found in MOMA object json:
+        // MOMA object url =  https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
+        // ---- img src, 
+        // ---- title, 
+        // ---- artist name, 
+        // ---- medium, 
+        // ---- link to moma/wiki entry
+        // -- display element on page
 
 const galleryApp = {};
 
